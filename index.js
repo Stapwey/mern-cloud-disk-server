b03223bdfa22ab1059c4cd57a248ca7ece1371b2
Dmitriy Semenov <semenov.ds12@gmail.com>
2023-05-10 23:05:47 +0300

    Database connection created

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+const { Schema, connect } = require("mongoose");
+const authRouter = require("./routes/auth.routes")
+const app = express()
+const PORT = config.get('serverPort')
+
+app.use(express.json())
+app.use("/api/auth", authRouter)
+
+const start = async () => {
+    try {
+        await connect(config.get("dbUrl"))
+
+        app.listen(PORT, () => {
+            console.log('Server started on port ', PORT)
+        })
+    } catch (e) {
+
+    }
+}
+
+start()
