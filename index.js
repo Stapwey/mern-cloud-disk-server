8d1273b66969f6df8520b069ce35740dd538b5b9
Dmitriy Semenov <semenov.ds12@gmail.com>
2023-05-11 14:34:01 +0300

    add dir creating

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const config = require("config")
 const { Schema, connect } = require("mongoose");
 const authRouter = require("./routes/auth.routes")
+const fileRouter = require("./routes/file.routes")
 const app = express()
 const PORT = config.get('serverPort')
 const corsMiddleware = require('./middleware/cors.middleware')
@@ -10,6 +11,7 @@ const corsMiddleware = require('./middleware/cors.middleware')
 app.use(corsMiddleware)
 app.use(express.json())
 app.use("/api/auth", authRouter)
+app.use("/api/files", fileRouter)
 
 const start = async () => {
     try {
