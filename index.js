7ba0a3944f576b487dce07ff44746c4d36154009
Dmitriy Semenov <semenov.ds12@gmail.com>
2023-05-11 19:19:51 +0300

    add upload file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
-const { Schema, connect } = require("mongoose");
+const fileUpload = require("express-fileupload")
+const { Schema, connect } = require("mongoose")
 const authRouter = require("./routes/auth.routes")
 const fileRouter = require("./routes/file.routes")
 const app = express()
 const PORT = config.get('serverPort')
 const corsMiddleware = require('./middleware/cors.middleware')
 
+
+app.use(fileUpload({}))
 app.use(corsMiddleware)
 app.use(express.json())
 app.use("/api/auth", authRouter)
@@ -15,13 +18,16 @@ app.use("/api/files", fileRouter)
 
 const start = async () => {
     try {
-        await connect(config.get("dbUrl"))
+        await connect(config.get("dbUrl"), {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
 
         app.listen(PORT, () => {
             console.log('Server started on port ', PORT)
         })
     } catch (e) {
-
+        console.log(e)
     }
 }
 
